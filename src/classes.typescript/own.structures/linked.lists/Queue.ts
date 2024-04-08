8e7e4888a4f0ab51fc203681d6cfe805ffa0b918
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-04-08 16:34:12 -0500

    Queue class is created.

--- src/classes.typescript/own.structures/linked.lists/Queue.ts
+++ src/classes.typescript/own.structures/linked.lists/Queue.ts
@@ -49,6 +49,76 @@ export class Queue<T> {
         return this.size === null;
     }
 
+    /**
+     * Delete all the elements of the Queue
+     */
+    public deleteQueue(): void {
+        this.firstNode = null;
+        this.lastNode = null;
+        this.size = 0;
+    }
+
+    /**
+     * 
+     * @returns the first element of the Queue
+     */
+    public getFirstNode(): Node<T> | null {
+        return this.firstNode;
+    }
+
+    /**
+     * 
+     * @returns the last element of the Queue
+     */
+    public getLastNode(): Node<T> | null {
+        return this.lastNode;
+    }
+
+    /**
+     * 
+     * @returns the size of the Queue
+     */
+    public getSize(): number {
+        return this.size;
+    }
+
+    /**
+     * 
+     * @param queue 
+     * @returns 
+     */
+    public sameQueue(queue: Queue<T>) : boolean {
+        if(this.size !== queue.getSize()){
+            return false;
+        }
+
+        let node : Node<T> | null = this.firstNode;
+        let node2 : Node<T> | null = queue.getFirstNode();
+        while(node !== null){
+            if(node.getData() !== node2?.getData()){
+                return false;
+            }
+            node = node.getNextNode();
+            node2 = node2?.getNextNode();
+        }
+        return true; 
+    }
+
+    /**
+     * Print the elements of the Queue
+     */
+    public print(): void {
+        let node: Node<T> | null = this.firstNode;
+        while(node !== null){
+            console.log(node.getData());
+            node = node.getNextNode();
+        }
+    }
+
+    /**
+     * 
+     * @returns a copy of the Queue
+     */
     protected clone: () => Queue<T> = () => {
         const queue: Queue<T> = new Queue<T>();
         let node: Node<T> | null = this.firstNode;
