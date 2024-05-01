0ceb2eb80bd7640517e2ea2bb7ae18445ba1cbcf
YarleyMejia <100544029+YarleyMejia@users.noreply.github.com>
2024-05-01 09:42:49 -0500

    Prueba de html

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,17 @@
 import {createBrowserRouter} from 'react-router-dom';
 import Layout from './views/layout/Layout';
+import WallPost from './views/components/WallPost';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout />
+        element: <Layout />,
+        children: [
+            {
+                path: '/suggestions',
+                element: <WallPost />
+            }
+        ]
     }
 ]);
 
